56a756e76ef5ee70900e7268da0bd79bd8dd7201
Dominik <dominik.galczyk@gmail.com>
2024-10-08 08:59:51 +0200

    trzymanie stanu wykresu

--- src/Plots/MyPlot.js
+++ src/Plots/MyPlot.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Plot from 'react-plotly.js';
 
+const layout = {
+    paper_bgcolor: '#282c34',
+    plot_bgcolor: '#282c34',
+    xaxis: {
+        title: 'Time [s]',
+        color: 'white',
+        gridcolor: '#444',
+    },
+    yaxis: {
+        title: 'Position [cm]',
+        color: 'white',
+        gridcolor: '#444',
+    },
+    title: {
+        text: 'Position of the rotor',
+        font: {
+            size: 20,
+            color: 'white'
+        }
+    }
+};
+
 const MyPlot = ({y}) => {
+    const [plotLayout, setPlotLayout] = useState(layout);
     const plotData =
         [
             {
@@ -13,32 +36,29 @@ const MyPlot = ({y}) => {
             }
         ];
 
-    const layout = {
-        paper_bgcolor: '#282c34',
-        plot_bgcolor: '#282c34',
-        xaxis: {
-            title: 'Time [s]',
-            color: 'white',
-            gridcolor: '#444',
-        },
-        yaxis: {
-            title: 'Position [cm]',
-            color: 'white',
-            gridcolor: '#444',
-        },
-        title: {
-            text: 'Position of the rotor',
-            font: {
-                size: 20,
-                color: 'white'
-            }
+    const handleUpdate = (figure) => {
+        if (
+            figure.layout.xaxis.range[0] === plotLayout.xaxis.range[0]
+            && figure.layout.xaxis.range[1] === plotLayout.xaxis.range[1]
+            && figure.layout.yaxis.range[0] === plotLayout.yaxis.range[0]
+            && figure.layout.yaxis.range[1] === plotLayout.yaxis.range[1]
+        ) {
+            return;
         }
+
+        setPlotLayout((prevState) => {
+           return {
+               ...prevState,
+               ...figure,
+           };
+        });
     };
 
     return (
         <Plot
             data={plotData}
-            layout={layout}
+            layout={plotLayout}
+            onUpdate={handleUpdate}
         />
     );
 };
